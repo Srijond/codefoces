97070ac3b89f0061ce3b3df0d19f685263a419a7
srijond <srijonrahman00@gmail.com>
2019-06-04 01:55:37 +0600

    contest 1st pblm solve in my lifediv2.cpp

---
+++ div2.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[1001],i,n,j,sum=0,sum1=0;
+    cin>>n;
+    for(i=0;i<(2*n);i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+(2*n));
+    for(i=0;i<n;i++)
+    {
+        sum+=a[i];
+    }
+    for(j=i;j<(2*n);j++)
+    {
+        sum1+=a[j];
+    }
+    if(sum!=sum1)
+    {
+       for(i=0;i<(2*n);i++)
+       {
+           cout<<a[i]<<" ";
+       }
+    }
+    else
+        cout<<"-1";
+
+
+
+
+
+
+
+
+    return 0;
+}
